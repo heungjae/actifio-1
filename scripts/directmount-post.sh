d47556a4f678d927f5fc746a7424f5703fd957fd
chewcfoo <34571329+chewcfoo@users.noreply.github.com>
2019-04-27 12:01:15 +1000

    Create directmount-post.sh

---
+++ scripts/directmount-post.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## File: dm-post.sh
+#
+set +x
+
+LOG_FILE=/act/scripts/trace.log
+
+echo "** `date \"+%Y.%m.%d-%H.%M.%S\"` Executing script ( $0 ) **" >> $LOG_FILE
+
+if [[ -z "$ACT_MULTI_END" ]] && [[ $ACT_MULTI_END != "true" ]]; then
+   echo "NO-OP for multi-phase operation"
+   exit 0
+fi
+
+if [[ $ACT_PHASE == "post" && $ACT_MULTI_OPNAME == "mount" && $ACT_MULTI_END == "true" ]]; then
+    if [[ $ACT_JOBTYPE == "reprovision" || $ACT_JOBTYPE == "mount" ]] ; then
+       echo "Database is up and running ... restore data into the database !" >> $LOG_FILE
+       echo "--------------------------------------------------------------" >> $LOG_FILE
+       ps -ef | grep pmon >> $LOG_FILE
+       exit 0
+    fi
+fi
